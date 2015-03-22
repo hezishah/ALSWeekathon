ac79e75f171d0363f5a5e174a4a64d396ba677fe
Ran Mizrachi <mizrachiran@gmail.com>
2015-03-22 16:24:01 +0200

    Save & retrieve signals from db by userid

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,44 @@
-var express = require('express'),
-	sensorData = require('./sensorData.js'),
-	port = 8080;
+var express = require('express'),	port = 8080;
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var monk = require('monk');
+var url = require('url');
+var db = monk('localhost:27017/als');
 
 var app = express();
-//var sample = sensorData.createSensorDataSample();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 
 app.get('/ping', function(req, res) {
 	res.send('OK');
 });
 
-app.post('/users/:userId/', function(req, res) {
-	// TODO, validate request and push data to the db
-    res.send('Post data for user: ' + req.params["userId"]);
+app.get('/signals/:userid', function(req, res){
+    var collection = db.get('signals');
+	var userid = req.params.userid;
+    collection.find({'userid' : userid },{},function(e, data){
+        res.send(data);
+    });	
+});
+
+app.post('/signals/:userid/',function(req, res){
+	if (!req.body) return res.sendStatus(400);
+	
+	var collection = db.get('signals');
+	var id = req.params.userid;
+	var data = req.body.signals;
+	
+	for(i=0;i<data.length;i++)
+	{
+		data[i].userid = id;
+		data[i].timestamp = new Date().getTime();	
+	}
+	
+	collection.insert(data, function(err, result) {
+		console.log("Signals saved successfully!");
+		res.send('OK');
+	});
 });
 
 var server = app.listen(port, function () {
