caae823a4776add991324f76635065c8c1911e61
Ran Mizrachi <mizrachiran@gmail.com>
2015-03-24 18:08:32 +0200

    add signals analysis code

---
+++ Server/analysis.js
@@ -0,0 +1,59 @@
+exports.process = function (data) {
+    var meanAcselV = 0;
+    var sensorAboveAverage = [];
+    for (var i = 0; i < data.length; i++) {
+        var signal = data[i];
+        console.log(signal);
+        signal.acsel3Square = signal.accelerometer.x * signal.accelerometer.x +
+                               signal.accelerometer.y * signal.accelerometer.y +
+                               signal.accelerometer.z * signal.accelerometer.z;
+        meanAcselV += (signal.acsel3Square) / data.length;
+    }
+    for (var i = 0; i < data.length; i++) {
+        var signal = data[i];
+        if (signal.acsel3Square > meanAcselV) {
+            var stepHit = { timeMs: signal.timestamp, acsel3Square: signal.acsel3Square }
+            sensorAboveAverage.push(stepHit);
+        }
+        else {
+            var stepHit = { timeMs: signal.timestamp, acsel3Square: 0 }
+            sensorAboveAverage.push(stepHit);
+        }
+    }
+    var waitCount = 0, walkCount = 0, waitStartTime = sensorAboveAverage[0].timeMs, waitEndTime = sensorAboveAverage[0].timeMs;
+    var lastWaitTime = -1;
+    var waitStartTimeIndex = -1;
+    var stepTimeList = [];
+    for (var i = 0; i < sensorAboveAverage.length; i++) {
+        // summ of time of continuies continues zero and calcualte
+        if (sensorAboveAverage[i].acsel3Square == 0) {
+            if (waitCount == 0) {
+                if (walkCount != 1) {
+                    waitStartTime = sensorAboveAverage[i].timeMs;
+                    waitStartTimeIndex = i;
+                }
+            }
+            walkCount = 0;
+            waitCount++;
+        }
+        else {
+            walkCount++;
+            waitEndTime = sensorAboveAverage[i].timeMs;
+            if (waitCount > -3) {
+                if (lastWaitTime == waitStartTime) {
+                    stepTimeList[stepTimeList.length - 1].stepDuration = waitEndTime - waitStartTime;
+                }
+                else {
+                    var stepTime = { timeStamp: waitStartTime, stepDuration: waitEndTime - waitStartTime };
+                    stepTimeList.push(stepTime);
+                }
+
+                lastWaitTime = waitStartTime;
+            }
+            waitCount = 0;
+        }
+    }
+
+
+    return stepTimeList;
+}
--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,13 @@ var db = monk('localhost:27017/als');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
+var analysis = require('./analysis.js');
 
 app.get('/ping', function (req, res) {
     res.send('OK');
@@ -35,20 +37,26 @@ app.get('/signals/:userid', function (req, res) {
     });
 });
 
-app.get('/signals/:userid/:from/:to', function (req, res) {
+app.get('/signals/:userid/:sensor/:from/:to', function (req, res) {
     var collection = db.get('signals');
     var userid = req.params.userid;
     if (!userid) return res.sendStatus(400);
 
     var from = req.params.from;
     var to = req.params.to;
+    var sensor = req.params.sensor;
 
-    if (!from || !to) return res.sendStatus(400);
+    if (!from || !to || !sensor) return res.sendStatus(400);
 
-    collection.find({ 'userid': userid, 'timestamp': { $gt: from, $lt: to } }, {}, function (e, data) {
+    collection.find({ 'userid': userid, 'sensorname': sensor, 'uploadtime': { $gte: parseInt(from), $lte: parseInt(to) } }, {}, function (e, data) {
+        if (data) {
+            console.log('received ' + data.length + ' items back');
+        }
+
         res.send(data);
     });
 });
+
 
 app.post('/signals/:userid/', function (req, res) {
     if (!req.body) return res.sendStatus(400);
@@ -77,11 +85,10 @@ app.post('/signals/:userid/', function (req, res) {
         return res.status(400).json({ error: 'sensors json is missing or invalid' }).send();
     }
 
-    var sensorName = req.body.sensors.name;
-    var data = req.body.sensors.data;
+    var sensorsArr = req.body.sensors;
 
-    if (!data || data.length == 0) {
-        console.log('post signals - sensors data is missing or invalid');
+    if (!sensorsArr || sensorsArr.length == 0) {
+        console.log('post signals - sensors array is missing or invalid');
         return res.status(400).json({ error: 'sensors data is missing or invalid' }).send();
     }
 
@@ -90,16 +97,22 @@ app.post('/signals/:userid/', function (req, res) {
         return res.status(400).json({ error: 'upload timestamp is missing or invalid' }).send();
     }
 
-    if (!sensorName) {
-        console.log('post signals - sensor name is missing or invalid');
-        return res.status(400).json({ error: 'sensor name is missing or invalid' }).send();
+    var data = [];
+
+    for (var i = 0; i < sensorsArr.length; i++) {
+        for (var j = 0; j < sensorsArr[i].data.length; j++) {
+
+            var item = sensorsArr[i].data[j];
+
+            item.userid = id;
+            item.uploadtime = uploadTime;
+            item.sensorname = sensorsArr[i].name;
+
+            data.push(item);
+        }
     }
 
-    for (i = 0; i < data.length; i++) {
-        data[i].userid = id;
-        data[i].uploadtime = uploadTime;
-        data[i].sensorname = sensorName;
-    }
+    console.log(data.length);
 
     signalscollection.insert(data, function (err, result) {
         console.log("Signals saved successfully!");
@@ -194,6 +207,42 @@ app.post('/validate/', function (req, res) {
         res.send(data);
     });
 });
+
+app.get('/analysis/:userid/:timestamp', function (req, res) {
+    var collection = db.get('signals');
+    var userid = req.params.userid;
+    if (!userid) {
+        console.log('user id is missing')
+        return res.sendStatus(400);
+        return res.status(400).json({ error: 'user id is missing or invalid' }).send();
+    }
+
+    var timestamp = req.params.timestamp;
+
+    if (!timestamp) {
+        console.log('timestamp is missing')
+        return res.status(400).json({ error: 'timestamp is missing or invalid' }).send();
+    }
+
+    collection.find({ 'userid': userid, 'sensorname': 'right', 'uploadtime': parseInt(timestamp) }, {}, function (e, data) {
+        if (data) {
+            console.log('received ' + data.length + ' items back');
+        }
+
+        var rightResult = analysis.process(data);
+
+        collection.find({ 'userid': userid, 'sensorname': 'left', 'uploadtime': parseInt(timestamp) }, {}, function (e, data) {
+            if (data) {
+                console.log('received ' + data.length + ' items back');
+            }
+
+            var leftResult = analysis.process(data);
+
+            return res.send({ right: rightResult, left: leftResult });
+        });
+    });
+});
+
 
 var server = app.listen(port, function () {
 
